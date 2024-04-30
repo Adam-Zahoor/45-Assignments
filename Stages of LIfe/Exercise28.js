f8cfa8c0e2ec913cc31d4ed2514b3e611bb4c151
Adam Zahoor <adamzahoor18@gmail.com>
2024-04-30 16:21:37 +0500

    first commit

---
+++ Stages of LIfe/Exercise28.js
@@ -0,0 +1,22 @@
+var personAge = 6;
+if (personAge < 2) {
+    console.log("You are a baby");
+}
+else if (personAge >= 2 && personAge < 4) {
+    console.log("You are a toddler");
+}
+else if (personAge >= 4 && personAge < 13) {
+    console.log("You are a kid");
+}
+else if (personAge >= 13 && personAge < 20) {
+    console.log("You are a teenager");
+}
+else if (personAge >= 20 && personAge < 65) {
+    console.log("You are an adult");
+}
+else if (personAge >= 65) {
+    console.log("You are elder");
+}
+else {
+    console.log("Please enter a valid input");
+}
